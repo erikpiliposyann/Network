1095b1af506733537acbc756ee86dde9c664aafb
erikpiliposyann <erikpiliposyann@gmail.com>
2026-06-06 16:02:49 +0400

    Implement basic TCP client in client.c

--- PorjectTCP/client.c
+++ PorjectTCP/client.c
@@ -1,1 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main(){
+    int client_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(client_fd < 0){
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in client_addr;
+
+    client_addr.sin_family = AF_INET;
+    client_addr.sin_port = htons(PORT);
+
+    inet_pton(AF_INET, "127.0.0.1", &client_addr.sin_addr);
+    socklen_t client_size = sizeof(client_addr);
+
+    struct timeval timeout;
+    timeout.tv_sec = 5;
+    timeout.tv_usec = 0;
+    setsockopt(client_fd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
+
+    if(connect(client_fd, (struct sockaddr*) &client_addr, client_size) < 0){
+        perror("connect");
+        exit(EXIT_FAILURE);
+    }
+
+    char command[BUFFER_SIZE] = {0};
+    fgets(command, BUFFER_SIZE, stdin);
+    command[strcspn(command, "\n")] = '\0'; 
+    send(client_fd, command, strlen(command), 0);
+
+    char response[BUFFER_SIZE] = {0};
+    recv(client_fd, response, BUFFER_SIZE - 1, 0);
+    printf("%s", response);
+
+    close(client_fd);
+
+    return 0;
+}
